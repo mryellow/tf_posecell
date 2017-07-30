d1f5647762ba1c168584c24944446ce9bbb23626
Mr-Yellow <mr-yellow@mr-yellow.com>
2017-07-31 07:11:05 +1000

    Clear ground

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
 ## RatSLAM PoseCell in TensorFlow
 
-`pca_new[zw][yw][xw] += posecells[z][y][x] * PC_W_EXCITE[excite_index++];`
-
-https://github.com/mryellow/ratslam/blob/ratslam_ros/src/ratslam/posecell_network.cpp#L710
-
-That's meant to be adding to surrounding cells. Rather than moving the distribution to those coordinates.
+WIP
--- main.py
+++ main.py
@@ -43,75 +43,6 @@ PC_VT_INJECT_ENERGY = 0.15
 PC_VT_RESTORE = 0.05
 VT_ACTIVE_DECAY = 1.0
 
-const_global_inhibit = tf.fill(shape, PC_GLOBAL_INHIB, name="ConstGlobalInhibit")
-const_active_decay = tf.fill(shape, VT_ACTIVE_DECAY, name="ConstGlobalDecay")
-
-pose_input = tf.placeholder(tf.int64, shape=in_shape, name="PoseInput")
-view_input = tf.placeholder(tf.int64, shape=in_shape, name="ViewInput")
-posecells  = tf.Variable(tf.ones(shape, tf.float32), name="PoseCells")
-view_decay = tf.Variable(tf.fill(shape, VT_ACTIVE_DECAY), name="ViewDecay")
-
-# Generate PDFs for excite/inhibit spreading
-excite_pdf = gen_pdf(1, dim, dim_mid)
-inhibit_pdf = gen_pdf(2, dim, dim_mid)
-
-# FIXME: Moving the PDF to known location and using it to shape the normalisation rather than "spread" the energy.
-# To roll the PDF like this to spread for every position is a bit much.
-# Start with a smaller shape, perhaps can convolve it with TF somehow.
-
-# Roll the PDFs until aligned with current index and reshape
-rolled_excite_pdf = tf.py_func(np.roll, [excite_pdf, ((pose_input[2]-dim/2) * dim * dim) + ((pose_input[1]-dim/2) * dim) + ((pose_input[0]-dim/2) + 1)], tf.float32, name="roll_excite")
-rolled_inhibit_pdf = tf.py_func(np.roll, [inhibit_pdf, ((pose_input[2]-dim/2) * dim * dim) + ((pose_input[1]-dim/2) * dim) + ((pose_input[0]-dim/2) + 1)], tf.float32, name="roll_inhibit")
-shaped_excite_pdf = tf.reshape(rolled_excite_pdf, shape, name="ExcitePDF")
-shaped_inhibit_pdf = tf.reshape(rolled_inhibit_pdf, shape, name="InhibitPDF")
-
-# Initalise globals
-tf.global_variables_initializer().run(session=sess)
-
-# Add to decay, except for current view
-# https://github.com/mryellow/ratslam/blob/ratslam_ros/src/ratslam/posecell_network.cpp#L1043
-decay_current = view_decay[view_input[0], view_input[1], view_input[2]]
-decay_added = tf.assign(view_decay, tf.add(view_decay, VT_ACTIVE_DECAY))
-# Replace current view with original decay
-decay_update = tf.scatter_nd_update(view_decay, [view_input], [decay_current])
-
-# Calculate energy given decay
-# https://github.com/mryellow/ratslam/blob/ratslam_ros/src/ratslam/posecell_network.cpp#L1047
-energy = tf.divide(PC_VT_INJECT_ENERGY * 1.0, tf.multiply(30.0, tf.subtract(30.0, tf.exp(tf.multiply(1.2, decay_update)))), name="CalcEnergy")
-
-# Inject energy given by decay, across all indexes
-inject = tf.add(posecells, energy, name="Inject")
-
-# Slightly restore decay
-# https://github.com/mryellow/ratslam/blob/ratslam_ros/src/ratslam/posecell_network.cpp#L1063
-decay_restore = tf.subtract(view_decay, PC_VT_RESTORE)
-
-# Limit decay to VT_ACTIVE_DECAY
-# https://github.com/mryellow/ratslam/blob/ratslam_ros/src/ratslam/posecell_network.cpp#L1065
-decay_applied = tf.subtract(decay_restore, const_active_decay, name="DecayRestore")
-decay_limited = tf.where(tf.greater_equal(decay_restore, const_active_decay), x=decay_applied, y=const_active_decay, name="DecayLimit")
-decay_update = tf.assign(view_decay, decay_limited)
-
-#inject = tf.scatter_nd_add(posecells, [pose_input], [energy], name="Inject")
-
-# FIXME: Because `shaped_excite_pdf` is tied to location, `excite` can't be called without a location.
-# FIXME: Should be looping `excite` even when no view is being injected
-# FIXME: `excite` should take every element and apply updates to those surrounding based on `shaped_excite_pdf`
-
-excite = tf.multiply(inject, shaped_excite_pdf, name="Excite")
-inhibit = tf.subtract(excite, tf.multiply(excite, shaped_inhibit_pdf), name="Inhibit")
-
-global_applied = tf.subtract(inhibit, const_global_inhibit, name="GlobalInhibit")
-global_limited = tf.where(tf.greater_equal(inhibit, const_global_inhibit), x=global_applied, y=inhibit)
-
-norm_posecells = tf.realdiv(global_limited, tf.reduce_sum(global_limited), name="Norm")
-#norm_posecells = tf.realdiv(inhibit, tf.reduce_sum(inhibit), name="Norm")
-
-# Reassign
-process_posecells = norm_posecells
-process_decays = decay_update
-#process = tf.assign(posecells, norm_posecells, name="Process")
-
 def main(_):
     writer = tf.summary.FileWriter("/tmp/tf_posecell_graph", sess.graph)
 
@@ -123,11 +54,7 @@ def main(_):
     def update(dt):
         print(window.x, window.y, window.z)
 
-        res = sess.run([process_posecells, process_decays], feed_dict={
-            pose_input: [window.x, window.y, window.z],
-            view_input: [window.x, window.y, window.z]
-        })
-        data = res[0]
+        # TODO: Do stuff
 
         # Re-scale between [0, 1] for rendering (transparency percentage)
         max_activation = 0
